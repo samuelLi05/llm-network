f2a8cee9e7734a8ebdf089a777e2437896ddb833
Caleb Probine <caleb.miles@hotmail.com>
2026-07-10 09:50:58 -0500

    Added tests for the rating-csv parsing code

--- verification/test_post_sampling.py
+++ verification/test_post_sampling.py
@@ -10,6 +10,11 @@ if str(ROOT) not in sys.path:
 
 
 from modeling.models.data_prep import _load_jsonl
+from verification.evaluate_rating_stance_fit import get_named_csvs, load_ratings_and_stance_scores
+
+RATING_DIR = ROOT / 'verification' / 'sampled_messages_with_ratings'
+TEMPLATE = 'sampled_message_survey_'
+
 
 SAMPLED_MESSAGE_LOCATION = ROOT / 'verification' / 'sampled_messages' / 'sampled_messages.jsonl'
 SAMPLED_MESSAGE_LOCATION_CSV = ROOT / 'verification' / 'sampled_messages' / 'sampled_messages.csv'
@@ -56,6 +61,37 @@ class TestPostSampling(unittest.TestCase):
                 loaded_messages_csv.append(row)
 
         self.assertEqual(loaded_messages_csv, loaded_messages_jsonl)
+
+    def test_message_match_post_ratings(self):
+
+        # validate that when we load csvs through ratings in the evaluate_rating_stance_fit experiment,
+        #  that the messages and stance scores match
+
+        f_names, annotators = get_named_csvs(TEMPLATE, RATING_DIR)
+
+        print(f"Found annotators : {annotators}")
+
+        for f_name in f_names:
+            ldd_data = load_ratings_and_stance_scores(RATING_DIR / f_name, embedding_analyzer=None)
+
+            print(f"In file {f_name}, found {len(ldd_data)} rated messages.")
+
+            for msg in ldd_data:
+
+                ldd_text = msg["text"]              # text in list we'll use for processing
+                ldd_stance = msg['stance_score']    # stance score we'll use for processing
+
+                # check that these values match the logged data from simulations
+                msg_with_align_fn = RUN_LOCATION / msg["dir_name"] / 'messages_with_alignment.jsonl'
+                self.assertTrue(msg_with_align_fn.exists())
+                msg_with_align_file = _load_jsonl(msg_with_align_fn)
+
+                lookup_text = msg_with_align_file[int(msg['index'])]['message']
+                lookup_stance = msg_with_align_file[int(msg['index'])]['published']['stance_score']
+
+                self.assertEqual(ldd_text, lookup_text)
+                self.assertEqual(ldd_stance, lookup_stance)
+
 
 if __name__ == "__main__":
     unittest.main()
